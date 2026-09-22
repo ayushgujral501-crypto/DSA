e113415a2df3330251050387b2ed7e84659c1e46
Ayush Gujral <ayushgujral501@gmail.com>
2026-09-22 19:54:59 +0530

    Add files via upload

---
+++ Quicksort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        int p = i + 1;
+
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = 6;
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
